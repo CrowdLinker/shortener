5a8832ed0913b0e9024d178d904dd120d1ac0c88
Meet Godhani <meet_godhani1@yahoo.com>
2014-06-18 15:16:00 -0400

    configured application

--- app/config/app.php
+++ app/config/app.php
@@ -78,7 +78,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'R8LwqTPYeIm6zwF8CAxbv2p2avr9eXuL',
 
 	'cipher' => MCRYPT_RIJNDAEL_128,
 
@@ -121,7 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-
+        'Way\Generators\GeneratorsServiceProvider',
 	),
 
 	/*
