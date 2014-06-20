94e29eda992c573fabeb5e22aae48d858d15b405
Meet Godhani <meet_godhani1@yahoo.com>
2014-06-20 09:42:29 -0400

    added model for shortlink

---
+++ app/models/ShortLink.php
@@ -0,0 +1,47 @@
+<?php
+
+class ShortLink extends \Eloquent {
+	protected $fillable = ['url','hash','pagetitle','domainprovider'];
+    protected $table = 'shortlinks';
+
+    /**
+     * Belongs to User
+     */
+    public function user()
+    {
+        return $this->belongsTo('User');
+    }
+
+    /**
+     * Refresh/remove cache on save & delete
+     */
+    public static function boot()
+    {
+        parent::boot();
+        static::saved(function()
+        {
+            Cache::flush();
+        });
+
+        static::created(function()
+        {
+            Cache::flush();
+        });
+
+        static::updating(function()
+        {
+            Cache::flush();
+        });
+
+        static::updated(function()
+        {
+            Cache::flush();
+        });
+
+        static::deleted(function()
+        {
+            Cache::flush();
+        });
+    }
+
+}
