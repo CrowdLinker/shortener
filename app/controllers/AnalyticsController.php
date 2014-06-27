ab9fe52cfb7b9906dabe8d09252ec0d526716409
Meet Godhani <meet_godhani1@yahoo.com>
2014-06-27 18:45:11 -0400

    fix

--- app/controllers/AnalyticsController.php
+++ app/controllers/AnalyticsController.php
@@ -57,7 +57,6 @@ class AnalyticsController extends ApiController {
     public function map($shortlink)
     {
         $details = Location::where('shortlink_id','=',$shortlink)->remember(10)->get();
-        return $details;
         $data = Shortener::mapLocation($details);
         return $this->setStatusCode(200)->respond($data);
     }
