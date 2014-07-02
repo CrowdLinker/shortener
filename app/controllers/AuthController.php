a1ca20107bb5556350f0706906685e11ed402a10
Meet Godhani <meet_godhani1@yahoo.com>
2014-07-02 14:29:03 -0400

    fix

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -140,7 +140,7 @@ class AuthController extends ApiController {
             //Get User Id and login
             Auth::login($user);
             $this->insertSession($result['id'],$token->getAccessToken(),'facebook');
-            return Redirect::intended('dashboard');
+            return Redirect::to(URL::previous());
         }
         // if not ask for permission first
         else {
