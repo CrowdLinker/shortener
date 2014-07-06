0e615a06a5e50ba3e35f0e93b59d4a9a49ee6922
Meet Godhani <meet_godhani1@yahoo.com>
2014-07-05 20:50:57 -0400

    fix

--- app/Crowdlinker/Facebook/Service.php
+++ app/Crowdlinker/Facebook/Service.php
@@ -12,7 +12,7 @@ class Service
     public function post($token,$data,$link)
     {
         $client = new Client(['base_url' => 'https://graph.facebook.com']);
-        $params = ['access_token' => $token,'message' => $data];
+        $params = ['access_token' => $token,'message' => $data,'link' => $link ];
         $request = $client->post('/v2.0/me/feed?'.http_build_query($params));
         $response = json_encode($request->json());
         return $response;
